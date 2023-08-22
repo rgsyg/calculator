320347c8d9aae92a7c73c2a495c96d7e66cb549e
rigs <rigorsyguatbsn@gmail.com>
2023-08-22 15:54:48 +0800

    feat: add functions in javascript

---
+++ script.js
@@ -0,0 +1,22 @@
+let firstOperand, secondOperand;
+let operator;
+
+function add() {
+
+}
+
+function subtract() {
+    
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function operate(operator, firstOperand, secondOperand) {
+
+}
