f1b7a11c64c3e1197fa6ed5b4b6e90a9abaa298a
rigs <rigorsyguatbsn@gmail.com>
2023-08-22 18:52:38 +0800

    feat: add working displays/functions

--- script.js
+++ script.js
@@ -1,22 +1,71 @@
+const display = document.querySelector(".calculator-display__wrapper");
+const buttons = document.querySelectorAll(".btnNum");
+const operatorButtons = document.querySelectorAll(".btnOperator");
+const equalsButton = document.querySelector(".btnEquals");
+
 let firstOperand, secondOperand;
 let operator;
 
-function add() {
-
+function add(firstOperand, secondOperand) {
+    display.textContent = +firstOperand + +secondOperand;
 }
 
-function subtract() {
-    
+function subtract(firstOperand, secondOperand) {
+    display.textContent = firstOperand - secondOperand;
 }
 
-function multiply() {
-
+function multiply(firstOperand, secondOperand) {
+    display.textContent = firstOperand * secondOperand;
 }
 
-function divide() {
-
+function divide(firstOperand, secondOperand) {
+    display.textContent = firstOperand / secondOperand;
 }
 
-function operate(operator, firstOperand, secondOperand) {
-
+function operate() {
+    if (!operator || !firstOperand || !secondOperand) return;
+    switch (operator) {
+        case '+':
+            add(firstOperand, secondOperand);
+            break;
+        case '-':
+            subtract(firstOperand, secondOperand);
+            break;
+        case '×':
+            multiply(firstOperand, secondOperand);
+            break;
+        case '/':
+            divide(firstOperand, secondOperand);
+            break;
+    }
+    clear();
 }
+
+function displayValue() {
+    if (operator) {
+        display.textContent = firstOperand;
+        display.textContent += ' ' + operator;
+
+        secondOperand = this.value;
+        display.textContent += ' ' + secondOperand;
+    } else {
+        firstOperand = this.value;
+        display.textContent = firstOperand;
+    }
+}
+
+function displayOperator() {
+    if (!firstOperand) return;
+    display.textContent = firstOperand;
+
+    operator = this.value;
+    display.textContent += ' ' + operator;
+}
+
+function clear() {
+    firstOperand, secondOperand, operator = null;
+}
+
+buttons.forEach((button) => button.addEventListener("click", displayValue));
+operatorButtons.forEach((operator) => operator.addEventListener("click", displayOperator));
+equalsButton.addEventListener("click", operate);
